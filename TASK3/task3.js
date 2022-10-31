fab19a62d7b3a815d72aba947917606858ffe9db
ArchanaCh2003 <98682999+ArchanaCh2003@users.noreply.github.com>
2022-10-31 18:08:18 +0530

    Add files via upload
    Web development and designing internship.(Oasis)
    Level-2

---
+++ TASK3/task3.js
@@ -0,0 +1,49 @@
+let input = document.querySelector('.enter-items')
+let addbtn = document.querySelector('.add-items')
+let task = document.querySelector('.task-list')
+
+
+
+
+//add new items to the list  
+
+addbtn.addEventListener('click',() => {
+    let newdiv = document.createElement('div');
+    if(input.value.trim() != 0){
+        
+        newdiv.classList.add('task-items');
+        newdiv.innerHTML = `
+        <p> ${input.value} </p>
+        <div class="item-btn">
+            
+            <i id="w2" class="fa fa-check-circle"></i>
+            <i id="w3" class="fa fa-times-circle"></i>
+        </div>
+        `
+        task.appendChild(newdiv);
+        input.value = ' ';
+    }
+    else{
+        alert('This webapp requires you to enter a task!!!')
+    }
+})
+
+//delete an item from the list
+
+task.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-times-circle')) {
+        e.target.parentElement.parentElement.remove();
+    }
+
+})
+
+//mark item as completed
+
+task.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-check-circle')) {
+        e.target.parentElement.parentElement.classList.toggle('completed');
+        e.target.classList.toggle('completed');
+    }
+})
+
+
